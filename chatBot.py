00c3f1c7d0c4ac56e424944f4643585bf1322f9c
JorgeNascimento <j.luiznasciment@gmail.com>
2021-01-20 15:34:42 -0300

    Separação de perguntas e respostas e pré-processamento dos textos

--- chatBot.py
+++ chatBot.py
@@ -27,11 +27,40 @@ for linha in linhas:
 #lista de conversas
 conversas_id = []
 for conversa in conversas[:-1]:#-1 exclui o ultimo registro
+
   #print(conversa)
   #replace remove espaços vazios e aspas simples
  _conversa = conversa.split(' +++$+++ ')[-1][1:-1].replace("'", "").replace(" ", "")
- print(conversa)
- 
+  
  conversas_id.append(_conversa.split(","))
      
+#separação de perguntas e respostas
+perguntas = []
+respostas = []
 
+for conversa in conversas_id:
+  #print(conversa)
+  for i in range(len(conversa) - 1):
+    #print(conversa)
+    perguntas.append(id_para_linha[conversa[i]])
+    respostas.append(id_para_linha[conversa[i + 1]])
+
+
+#pré-processamento dos textos
+def limpa_texto(texto):
+  texto = texto.lower()
+  texto = re.sub(r"i'm", "i am", texto)
+  texto = re.sub(r"he's", "he is", texto)
+  texto = re.sub(r"she's", "she is", texto)
+  texto = re.sub(r"that's", "that is", texto)
+  texto = re.sub(r"what's", "what is", texto)
+  texto = re.sub(r"where's", "where is", texto)
+  texto = re.sub(r"\'ll", " will", texto)
+  texto = re.sub(r"\'ve", " have", texto)
+  texto = re.sub(r"\'re", " are", texto)
+  texto = re.sub(r"\'d", " would", texto)
+  texto = re.sub(r"won't", "will not", texto)
+  texto = re.sub(r"csn't", "cannot", texto)
+  texto = re.sub(r"[-()#/@;:<>[]{}~+=?.|,]", "", texto)
+  
+  return texto
