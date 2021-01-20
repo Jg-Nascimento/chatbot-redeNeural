ec3ed9322d8d184f4915e1cd0f23c665a3d83fe4
JorgeNascimento <j.luiznasciment@gmail.com>
2021-01-19 23:12:19 -0300

    Mapeamento de conversa aula-21

--- chatBot.py
+++ chatBot.py
@@ -6,10 +6,10 @@ import time
 
 #carregando base de dados
 #abre o arquivo, faz a leitura e pula linha onde tiver \n
-linhas=open('/recursos/movie_lines.txt', 
+linhas=open('/content/drive/MyDrive/Colab Notebooks/movie_lines.txt', 
               encoding='utf-8',errors='ignore').read().split('\n')
 
-linhas=open('/recursos/movie_conversations.txt', 
+conversas=open('/content/drive/MyDrive/Colab Notebooks/movie_conversations.txt', 
               encoding='utf-8',errors='ignore').read().split('\n')
 
 #dicionario para mapeamento de id's
@@ -19,3 +19,16 @@ for linha in linhas:
    # print(linha)
     _linha = linha.split(' +++$+++ ')
     print(linha)
+    
+     if len(_linha) == 5:
+      print (linha[4])
+      id_para_linha[_linha[0]] = _linha[4]
+
+#lista de conversas
+conversas_id = []
+for conversa in conversas[:-1]:#-1 exclui o ultimo registro
+  #print(conversa)
+ _conversa = conversa.split(' +++$+++ ')[-1]
+ print(conversa)
+    
+
